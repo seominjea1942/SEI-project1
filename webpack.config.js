9797e84591cf223b3733fe35ba11371a3201c0bb
seominjea1942 <seominjea1942@gmail.com>
2021-04-13 22:17:43 -0700

    for test deploying

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
     hot:true
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   plugins:[new webpack.HotModuleReplacementPlugin()],
   module: {
@@ -18,6 +18,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     ],
   },
 };
